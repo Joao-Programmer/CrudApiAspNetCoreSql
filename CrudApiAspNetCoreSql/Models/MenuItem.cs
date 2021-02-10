b77095afdbc847d8e9329a997a769d9fc7f77257
Joao-Programmer <joao_pr10@hotmail.com>
2021-02-10 13:25:47 -0300

    Adicionar arquivos de projeto.

---
+++ CrudApiAspNetCoreSql/Models/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApiAspNetCoreSql.Models
+{
+    public class MenuItem
+    {
+        public int id { get; set; }
+        public string description { get; set; }
+        public string large_portion_name { get; set; }
+        public string name { get; set; }
+        public decimal price_large { get; set; }
+        public decimal price_small { get; set; }
+        public string short_name { get; set; }
+        public string small_portion_name { get; set; }
+
+        public int categoryId { get; set; }
+        public Category category { get; set; }
+    }
+}
