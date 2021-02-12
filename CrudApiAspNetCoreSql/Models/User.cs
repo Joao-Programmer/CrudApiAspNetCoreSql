f4a0dd46fc1c69489c3901ca4dfbc014e520bbfd
Joao-Programmer <joao_pr10@hotmail.com>
2021-02-12 13:36:29 -0300

    Geração aumtomática do controller e context da classe categoria.

---
+++ CrudApiAspNetCoreSql/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudApiAspNetCoreSql.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserFullName { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserEmail { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UserCreateDate { get; set; }
+    }
+}
